6d588b80dbdac56327974e3171c08d6ee1d8a9d0
leemon <tacrapo@gmail.com>
2021-03-22 22:17:06 +0100

    Check if null

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,47 +1,49 @@
 document.addEventListener( 'DOMContentLoaded', function() {
 
     var calendarEl = document.getElementById( 'fullcalendar' );
-    var calendar = new FullCalendar.Calendar( calendarEl, {
-        locale: RSECAjax.locale.lang,
-        height: 'auto',
-        contentHeight: 'auto',
-        editable: false,
-        selectable: false,
-        headerToolbar: {
-            left: 'dayGridWeek,dayGridDay',
-            center: 'title',
-            right: 'today prev,next'
-        },
-        views: {
-            dayGrid: {
-                titleFormat: { year: 'numeric', month: 'long', day: 'numeric' },
-                dayHeaderFormat: { weekday: 'short', day: 'numeric' }
+    if ( calendarEl ) {
+        var calendar = new FullCalendar.Calendar( calendarEl, {
+            locale: RSECAjax.locale.lang,
+            height: 'auto',
+            contentHeight: 'auto',
+            editable: false,
+            selectable: false,
+            headerToolbar: {
+                left: 'dayGridWeek,dayGridDay',
+                center: 'title',
+                right: 'today prev,next'
             },
-            week: {
-                titleFormat: { year: 'numeric', month: 'long', day: 'numeric' },
-                dayHeaderFormat: { weekday: 'long', day: 'numeric' }
-            }
-        },
-        initialView: 'dayGridWeek',
-        fixedWeekCount: false,
-        firstDay: 1,
-        lazyFetching: 'true',
-        eventTextColor: '#fff',
-        defaultAllDay: true,
-        events: function( info, successCallback, failureCallback ) {
-            jQuery.ajax({
-                url: RSECAjax.ajaxurl + '?action=rsec-fullcal',
-                dataType: 'JSON',
-                data: {
-                    start: FullCalendar.formatDate( info.start.valueOf(), { year: 'numeric', month: '2-digit', day: '2-digit' } ),
-                    end: FullCalendar.formatDate( info.end.valueOf(), { year: 'numeric', month: '2-digit', day: '2-digit' } )
+            views: {
+                dayGrid: {
+                    titleFormat: { year: 'numeric', month: 'long', day: 'numeric' },
+                    dayHeaderFormat: { weekday: 'short', day: 'numeric' }
                 },
-                success: function( data ) {
-                    successCallback( data );
+                week: {
+                    titleFormat: { year: 'numeric', month: 'long', day: 'numeric' },
+                    dayHeaderFormat: { weekday: 'long', day: 'numeric' }
                 }
-            })
-        }
-    });
-    calendar.render();
+            },
+            initialView: 'dayGridWeek',
+            fixedWeekCount: false,
+            firstDay: 1,
+            lazyFetching: 'true',
+            eventTextColor: '#fff',
+            defaultAllDay: true,
+            events: function( info, successCallback, failureCallback ) {
+                jQuery.ajax({
+                    url: RSECAjax.ajaxurl + '?action=rsec-fullcal',
+                    dataType: 'JSON',
+                    data: {
+                        start: FullCalendar.formatDate( info.start.valueOf(), { year: 'numeric', month: '2-digit', day: '2-digit' } ),
+                        end: FullCalendar.formatDate( info.end.valueOf(), { year: 'numeric', month: '2-digit', day: '2-digit' } )
+                    },
+                    success: function( data ) {
+                        successCallback( data );
+                    }
+                })
+            }
+        });
+        calendar.render();
+    }
 
 });
